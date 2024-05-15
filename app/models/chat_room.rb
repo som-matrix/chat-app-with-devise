9dd8d1b62394d685f2992b49c1ee5973adfd5d57
som-matrix <swaroopsatya36@gmail.com>
2024-05-15 17:48:14 +0530

    chore: cleanup models and migrations

--- app/models/chat_room.rb
+++
@@ -1,7 +0,0 @@
-class ChatRoom < ApplicationRecord
-  has_many :participants
-  has_many :users, through: :participants
-  has_many :messages
-  validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
-end
