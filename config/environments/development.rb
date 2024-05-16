a6552d7d52c548f59814b5738af446ad923ea4d3
som-matrix <swaroopsatya36@gmail.com>
2024-05-16 14:06:37 +0530

    feat: add real-time update and delete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "inviting-divine-hawk.ngrok-free.app"
 end
